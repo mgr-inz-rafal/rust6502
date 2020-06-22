12f3a875d27c7ac1931cb34055ffccf854dc8746
mgr-inz-rafal <rchabowski@gmail.com>
2020-06-22 21:08:43 +0200

    Use 32-bit target

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+#![feature(llvm_asm)]
+
 use volatile_register::RW;
 #[repr(C)]
 pub struct COLOR {
     pub csr: RW<u8>
 }
 
-pub fn main() {
+pub fn black_box<T>(dummy: T) -> T {
+    unsafe { llvm_asm!("" : : "r"(&dummy)) }
+    dummy
+}
+
+#[inline(never)]
+pub fn asm6502() {
     let color = 764 as *const COLOR;
 
-    for i in 0u8..10 {
+    for i in 0u8..150 {
         unsafe {
             (*color).csr.write(i);
         }
     }
+}
+
+pub fn main() {
+    let _ = asm6502();
 }
